6e40f4cfeb30c09f32f52c680bb6c016deeb5a89
jgabrielmedina <jogabrielmedina@gmail.com>
2023-07-28 16:13:53 -0300

    cambios en el color del inicio

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -5,7 +5,7 @@ export const Layout = ( ) => {
     return(
         <>
               <div className="header">
-                <div><Link to="/">Home</Link></div>
+                <div><Link to="/">Inicio</Link></div>
               <div>   <Link to="/completadas">Tareas completadas</Link></div>
               </div>
               <Outlet/>
